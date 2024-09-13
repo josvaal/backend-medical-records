d18e0f927c92a05b571677b9a98f58bd8d952d2f
josval <josval.personal@gmail.com>
2024-09-13 09:36:08 -0500

    feat(model): Adición del modelo "Prescription"

---
+++ backend/src/main/java/com/josval/backend/model/entity/Prescriptions.java
@@ -0,0 +1,31 @@
+package com.josval.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "prescriptions")
+public class Prescriptions {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPrescription;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "medical_history_id",
+            referencedColumnName = "id"
+    )
+    private MedicalHistory medicalHistoryId;
+
+    @Column(name = "dose")
+    private String dose;
+
+    @Column(name = "duration")
+    private String duration;
+}
