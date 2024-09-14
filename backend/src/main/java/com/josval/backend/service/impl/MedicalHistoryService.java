58a48adb924d4bfee9f93ff89a928ec54168cdea
josval <josval.personal@gmail.com>
2024-09-14 13:06:29 -0500

    feat(medical history impl)

---
+++ backend/src/main/java/com/josval/backend/service/impl/MedicalHistoryService.java
@@ -0,0 +1,65 @@
+package com.josval.backend.service.impl;
+
+import com.josval.backend.model.dao.MedicalHistoryDAO;
+import com.josval.backend.model.dto.MedicalHistoryDTO;
+import com.josval.backend.model.entity.MedicalHistory;
+import com.josval.backend.service.IMedicalHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class MedicalHistoryService implements IMedicalHistoryService {
+    @Autowired
+    private MedicalHistoryDAO medicalHistoryDAO;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<MedicalHistory> listAll() {
+        return (List<MedicalHistory>) medicalHistoryDAO.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<MedicalHistory> listAllByPatientId(Integer patientId) {
+        return medicalHistoryDAO.findAllByPatientId(patientId);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<MedicalHistory> listAllByDoctorId(Integer doctorId) {
+        return medicalHistoryDAO.findAllByDoctorId(doctorId);
+    }
+
+    @Transactional
+    @Override
+    public MedicalHistory save(MedicalHistoryDTO medicalHistoryDTO) {
+        MedicalHistory medicalHistory = MedicalHistory.builder()
+                .idMedicalHistory(medicalHistoryDTO.getIdMedicalHistory())
+                .patientId(medicalHistoryDTO.getPatientId())
+                .doctorId(medicalHistoryDTO.getDoctorId())
+                .date(medicalHistoryDTO.getDate())
+                .diagnosis(medicalHistoryDTO.getDiagnosis())
+                .treatment(medicalHistoryDTO.getTreatment())
+                .build();
+        return medicalHistoryDAO.save(medicalHistory);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public MedicalHistory findById(Integer id) {
+        return medicalHistoryDAO.findById(id).orElse(null);
+    }
+
+    @Transactional
+    @Override
+    public void delete(MedicalHistory medicalHistory) {
+        medicalHistoryDAO.delete(medicalHistory);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public boolean existsById(Integer id) {
+        return medicalHistoryDAO.existsById(id);
+    }
+}
