0405a497b97b4e179a56e60d4c86e92848fd2f68
José Valentino <josvalmtrz@gmail.com>
2024-09-21 18:55:37 -0500

    feat(reversion): Remove filter, Add environment variables and new dependencies

--- backend/src/main/java/com/josval/backend/BackendApplication.java
+++ backend/src/main/java/com/josval/backend/BackendApplication.java
@@ -2,10 +2,17 @@ package com.josval.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+
+    System.setProperty("security.password", dotenv.get("SECURITY_PASSWORD"));
+    System.setProperty("security.token", dotenv.get("SECURITY_TOKEN"));
+    System.setProperty("security.token_validity", dotenv.get("SECURITY_TOKEN_VALIDITY"));
+
     SpringApplication.run(BackendApplication.class, args);
   }
 }
