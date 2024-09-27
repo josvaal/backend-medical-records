e48b8fa3b8aee8369b33e419044bcaac66be5422
José Valentino <josvalmtrz@gmail.com>
2024-09-26 22:43:10 -0500

    only backend

--- frontend/src/pages/prescription.jsx
+++
@@ -1,8 +0,0 @@
-export function Prescription(props) {
-  return (
-    <>
-      <h1 className="text-xl">Prescription Page</h1>
-      <h1 className="text-lg">{props.id}</h1>
-    </>
-  )
-}
--- frontend/src/pages/prescriptions.jsx
+++
@@ -1,7 +0,0 @@
-export function Prescriptions() {
-  return (
-    <>
-      <h1 className="text-xl">Prescriptions Page</h1>
-    </>
-  )
-}
