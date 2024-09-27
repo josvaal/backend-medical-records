9eacdd62ac5dcf1c99ea7d5f37e7f08b22811fac
José Valentino <josvalmtrz@gmail.com>
2024-09-26 22:54:44 -0500

    feat(properties)
    Change the properties

--- backend/src/main/java/com/josval/backend/BackendApplication.java
+++ backend/src/main/java/com/josval/backend/BackendApplication.java
@@ -9,6 +9,10 @@ public class BackendApplication {
   public static void main(String[] args) {
     Dotenv dotenv = Dotenv.load();
 
+    System.setProperty("server.port", dotenv.get("PORT"));
+    System.setProperty("spring.datasource.username", dotenv.get("DB_USER"));
+    System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+    System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
     System.setProperty("security.jwt.secret_key", dotenv.get("JWT_SECRET_KEY"));
     System.setProperty("security.jwt.expiration_time", dotenv.get("JWT_EXPRIATION_TIME"));
     System.setProperty("security.jwt.token_issuer", dotenv.get("JWT_TOKEN_ISSUER"));
