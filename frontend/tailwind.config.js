f6f16a57ff85ee84eaea7009d41cbd15bd5bf12d
José Valentino <josvalmtrz@gmail.com>
2024-10-17 00:05:23 -0500

    React + Tailwind + Tailwind Material UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+})
+
